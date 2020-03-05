6bd7e74e00e8f5b513e2d964ac11c2317df01470
Sanders Alex <sanders_alex@mail.ru>
2020-03-05 19:19:14 +0300

    Update

--- OctoBot.Tentacles.ForumEvaluator/RedditForumEvaluator.cs
+++ OctoBot.Tentacles.ForumEvaluator/RedditForumEvaluator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using OctoBot.AppServices;
 using OctoBot.Evaluator.Social;
 
@@ -6,19 +7,62 @@ namespace OctoBot.Tentacles
 {
 	public class RedditForumEvaluator : ForumSocialEvaluator, IDispatcherAbstractClient
 	{
-		protected override void EvalImpl()
+		public RedditForumEvaluator()
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
 		}
 
+		private void SetDispatcher(object dispatcher)
+		{
+			Debug.WriteLine(1);
+		}
+		private static void GetDispatcherClass()
+		{
+			Debug.WriteLine(1);
+		}
+		private void PrintEntry(object entry_text, object entry_note, string count = "")
+		{
+			Debug.WriteLine(1);
+		}
+		async private void ReceiveNotificationData(object data)
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetEvalNote()
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetSentiment(object entry)
+		{
+			Debug.WriteLine(1);
+		}
+		private void IsInterestedByThisNotification(object notification_description)
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetConfigElements(object key)
+		{
+			Debug.WriteLine(1);
+		}
+		private void FormatConfig()
+		{
+			Debug.WriteLine(1);
+		}
+		private void Prepare()
+		{
+			Debug.WriteLine(1);
+		}
 		protected override void GetData()
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
 		}
-
-		protected override void StartTask()
+		async protected override void EvalImpl()
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
+		}
+		async protected override void StartTask()
+		{
+			Debug.WriteLine(1);
 		}
 	}
 }
