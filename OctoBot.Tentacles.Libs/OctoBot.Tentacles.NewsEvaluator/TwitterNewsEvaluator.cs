e4966a1683a54901aa51974d959e65c2ce01c74b
Sanders Alex <sanders_alex@mail.ru>
2020-03-05 20:41:25 +0300

    RemoveGarbage

---
+++ OctoBot.Tentacles.Libs/OctoBot.Tentacles.NewsEvaluator/TwitterNewsEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using OctoBot.AppServices;
+using OctoBot.Evaluator.Social;
+
+namespace OctoBot.Tentacles
+{
+	public class TwitterNewsEvaluator : NewsSocialEvaluator, IDispatcherAbstractClient
+	{
+		public TwitterNewsEvaluator()
+		{
+			Debug.WriteLine(1);
+		}
+
+		protected override void EvalImpl()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void GetData()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void StartTask()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
