4d2ae0ce8c96aec052ccb459191b37e6069161f1
Sanders Alex <sanders_alex@mail.ru>
2020-03-05 21:08:15 +0300

    Social

--- OctoBot.Tentacles.Libs/OctoBot.Tentacles.NewsEvaluator/TwitterNewsEvaluator.cs
+++ OctoBot.Tentacles.Libs/OctoBot.Tentacles.NewsEvaluator/TwitterNewsEvaluator.cs
@@ -12,19 +12,65 @@ namespace OctoBot.Tentacles
 			Debug.WriteLine(1);
 		}
 
-		protected override void EvalImpl()
+		private void SetDispatcher(object dispatcher)
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
 		}
-
+		private static void GetDispatcherClass()
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetTwitterService()
+		{
+			Debug.WriteLine(1);
+		}
+		private void PrintTweet(object tweet_text, object tweet_url, object note, string count = "")
+		{
+			Debug.WriteLine(1);
+		}
+		async private void ReceiveNotificationData(object data)
+		{
+			Debug.WriteLine(1);
+		}
+		async private void CheckEvalNote(object data)
+		{
+			Debug.WriteLine(1);
+		}
+		private static void ComputeNotificationTimeToLive(object evaluation)
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetTweetSentiment(object tweet, object tweet_text, bool is_a_quote = false)
+		{
+			Debug.WriteLine(1);
+		}
+		private void IsInterestedByThisNotification(object notification_description)
+		{
+			Debug.WriteLine(1);
+		}
+		private void GetConfigElements(object key)
+		{
+			Debug.WriteLine(1);
+		}
+		private void FormatConfig()
+		{
+			Debug.WriteLine(1);
+		}
+		private void Prepare()
+		{
+			Debug.WriteLine(1);
+		}
 		protected override void GetData()
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
 		}
-
-		protected override void StartTask()
+		async protected override void EvalImpl()
 		{
-			throw new NotImplementedException();
+			Debug.WriteLine(1);
+		}
+		async protected override void StartTask()
+		{
+			Debug.WriteLine(1);
 		}
 	}
 }
---
+++ OctoBot.Tentacles.Libs/OctoBot.Tentacles.StatsEvaluator/GoogleTrendStatsEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using OctoBot.Evaluator.Social;
+
+namespace OctoBot.Tentacles
+{
+	public class GoogleTrendStatsEvaluator : StatsSocialEvaluator
+	{
+		public GoogleTrendStatsEvaluator()
+		{
+			Debug.WriteLine(1);
+		}
+
+		protected override void GetData()
+		{
+			Debug.WriteLine(1);
+		}
+		async protected override void EvalImpl()
+		{
+			Debug.WriteLine(1);
+		}
+		private void LoadConfig()
+		{
+			Debug.WriteLine(1);
+		}
+		private void SetDefaultConfig()
+		{
+			Debug.WriteLine(1);
+		}
+		async protected override void StartTask()
+		{
+			Debug.WriteLine(1);
+		}
+	}
+}
